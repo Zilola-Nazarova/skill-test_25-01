71fa589a6dc882f8e1194d8fea3e674302d0f049
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-01-25 22:16:33 +0500

    Create mountainData array of objects to dinamically change the values on the website

---
+++ src/components/mountainData.js
@@ -0,0 +1,59 @@
+const mountainData = [
+  {
+    title: 'Mountain 1',
+    schedule: [
+      {
+        date: '25 Nov 2016',
+        description: 'Vestibulum viverra',
+      },
+      {
+        date: '28 Nov 2016',
+        description: 'Vestibulum viverra',
+      },
+      {
+        date: '18 Dec 2016',
+        description: 'Vestibulum viverra',
+      },
+      {
+        date: '7 Jan 2016',
+        description: 'Vestibulum viverra',
+      },
+    ]
+  },
+  {
+    title: 'Mountain 2',
+    schedule: [
+      {
+        date: '25 Nov 2016',
+        description: 'Vestibulum viverra',
+      },
+      {
+        date: '28 Nov 2016',
+        description: 'Vestibulum viverra',
+      },
+    ]
+  },
+  {
+    title: 'Mountain 3',
+    schedule: [
+      {
+        date: '25 Nov 2016',
+        description: 'Vestibulum viverra',
+      },
+      {
+        date: '28 Nov 2016',
+        description: 'Vestibulum viverra',
+      },
+      {
+        date: '18 Dec 2016',
+        description: 'Vestibulum viverra',
+      },
+      {
+        date: '7 Jan 2016',
+        description: 'Vestibulum viverra',
+      },
+    ]
+  },
+];
+
+export default mountainData;
