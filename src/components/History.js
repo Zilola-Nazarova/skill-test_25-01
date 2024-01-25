21ab78b18567aee6629c3c47099eaf9366494f75
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-01-25 19:26:28 +0500

    Style the History component to match the given layout

--- src/components/History.js
+++ src/components/History.js
@@ -1,17 +1,16 @@
-import Carousel from './Carousel';
+import ImageCarousel from './ImageCarousel';
 import styles from '../styles/History.module.scss';
 
 const History = () => (
   <div id="history" className={styles.history}>
     <div className={styles.paragraph}>
       <h2>
-        <span>01.</span>
-        {/* <br/> */}
-        History
+        <span className="number">01.</span>
+        <span className="title">History</span>
       </h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in ante viverra, rutrum erat rutrum, consectetur mi. Proin at maximus est. Nullam purus ex, iaculis sed erat sed, blandit tincidunt quam. Cras scelerisque id quam sed dignissim Pellentesque urna nunc, gravida quis hendrerit ac, tristique ut quam. Vivamus suscipit dignissim tortor nec congue.</p>
     </div>
-    <Carousel />
+    <ImageCarousel />
   </div>
 );
 
